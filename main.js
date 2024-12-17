ec6835e2304169b96de6b6a40a453a257aecf6b9
WheresWalt <walter@walterit.net>
2024-12-17 16:18:58 -0500

    up

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 
 // Initialize the map
-let map = L.map('map').setView([39.630, -77.556], 5);
+let map = L.map('map').setView([39.40256, -76.61041], 16);
 // Load and display tile layer on the map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+let m1 = L.marker([39.40256, -76.61041]).addTo(map);
+m1.bindPopup("DeVeas Family Dentistry").openPopup();
